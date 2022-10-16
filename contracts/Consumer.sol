dbfe5d89df36a33c30febff7e0b01b09fc222df3
mcuv3 <MauricioAntonioMartinez@outlook.com>
2022-10-15 19:15:06 -0500

    test custom adapter

---
+++ contracts/Consumer.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import '@chainlink/contracts/src/v0.8/ChainlinkClient.sol';
+import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+contract ATestnetConsumer is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    uint256 private constant ORACLE_PAYMENT = 1 * LINK_DIVISIBILITY; // 1 * 10**18
+    string public gameStatus;
+ 
+
+    event RequestGameStatus(bytes32 indexed requestId, string id);
+    /**
+     *  Goerli
+     *@dev LINK address in Goerli network: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+     * @dev Check https://docs.chain.link/docs/link-token-contracts/ for LINK address for the right network
+     */
+    constructor() ConfirmedOwner(msg.sender) {
+        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+    }
+
+    function requestGameStatus() public onlyOwner {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            stringToBytes32("a9d26b606ebf427bb648c98c096137f3"),
+            address(this),
+            this.fulfillGameStatus.selector
+        );
+        sendChainlinkRequestTo(0x699d632F802ea13A4F3941E8e6C81C8bD0e40d25, req, ORACLE_PAYMENT);
+    }
+
+    function fulfillGameStatus(bytes32 _requestId, string memory _gameStatus) public recordChainlinkFulfillment(_requestId) {
+        emit RequestGameStatus(_requestId, _gameStatus);
+        gameStatus = _gameStatus;
+    }
+
+    function getChainlinkToken() public view returns (address) {
+        return chainlinkTokenAddress();
+    }
+
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(link.transfer(msg.sender, link.balanceOf(address(this))), 'Unable to transfer');
+    }
+
+    function cancelRequest(
+        bytes32 _requestId,
+        uint256 _payment,
+        bytes4 _callbackFunctionId,
+        uint256 _expiration
+    ) public onlyOwner {
+        cancelChainlinkRequest(_requestId, _payment, _callbackFunctionId, _expiration);
+    }
+
+    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            // solhint-disable-line no-inline-assembly
+            result := mload(add(source, 32))
+        }
+    }
+}
